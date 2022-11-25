2e546d7c7a37f664fb4fcabf928f96dbe4d19069
Vojan-Najov <serejah.mashichev@gmail.com>
2022-11-25 15:48:46 +0300

    add strrchr() function src and tests

---
+++ src/s21_strrchr.c
@@ -0,0 +1,19 @@
+#include "s21_string.h"
+
+char *s21_strrchr(const char *str, int c)
+{
+	char *ptr = NULL;
+
+	c = (unsigned char) c;
+	while (*str) {
+		if (*str == c) {
+			ptr = (char *) str;
+		}
+		++str;
+	}
+	if (c == '\0') {
+		ptr = (char *) str;
+	}
+
+	return (ptr);
+}
